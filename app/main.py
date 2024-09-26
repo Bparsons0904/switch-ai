529c7a3c038b879d5299392086db194be75c73e8
Bob Parsons <deadstylebp@gmail.com>
2024-09-26 07:01:48 -0500

    fix linting, update packages

--- app/main.py
+++ app/main.py
@@ -6,16 +6,20 @@ from .model import relevance_model
 app = FastAPI()
 
 
-class ReviewRequest(BaseModel):
-    review: str
-
-
 @app.get("/")
 async def root():
+    """Validation endpoint"""
     return {"message": "Hello World"}
+
+
+class ReviewRequest(BaseModel):
+    """Represents the reviews request body"""
+
+    review: str
 
 
 @app.post("/review")
 async def check_review_relevance(request: ReviewRequest):
+    """Handles the evaulation of reviews to check for relevancy"""
     relevance_score = relevance_model.predict_relevance(request.review)
     return {"relevance_score": relevance_score}
